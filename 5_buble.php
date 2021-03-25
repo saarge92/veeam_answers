5e539d8a248c6d941d2052da44e0b16ae3fb0af4
SERDAR DURDYEV <sdurdiev@code-pilots.ru>
2021-03-25 13:57:12 +0400

    Add bubble sort for 5 tasks

---
+++ 5_buble.php
@@ -0,0 +1,37 @@
+<?php
+// Здесь я попробую реализовать алгоритм пузырьковой сортировки в ручную
+
+require '5.php';
+
+
+$elementsArray = [
+    new Element(56, "Radom element"),
+    new Element(32, "Random element 2"),
+    new Element(57, "Random element 3")
+];
+
+// Реализация сортировкой
+function bubbleSort(array &$elements, string $fieldName, string $order = 'DESC'): void
+{
+    do {
+        $swapped = false;
+        for ($i = 1; $i < count($elements); $i++) {
+            $rightElement = $elements[$i];
+            $leftElement = $elements[$i - 1];
+            if ($leftElement->$fieldName > $rightElement->$fieldName) {
+                swap($elements, $i - 1, $i);
+                $swapped = true;
+            }
+        }
+    } while ($swapped != false);
+}
+
+function swap(array &$elements, int $indexLeft, int $indexRight)
+{
+    $temp = $elements[$indexLeft];
+    $elements[$indexLeft] = $elements[$indexRight];
+    $elements[$indexRight] = $temp;
+}
+
+bubbleSort($elementsArray, 'id');
+print_r($elementsArray);
