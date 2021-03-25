a884aef3639e54ed97d3f70a87120c797833f186
SERDAR DURDYEV <sdurdiev@code-pilots.ru>
2021-03-25 16:03:51 +0400

    Third task modified

--- 3.sql
+++ 3.sql
@@ -1,1 +1,3 @@
-select e.name from employes e group by e.name;
+# Идея с подзапросами
+select e.name,e.salary from employes e 
+where e.salary > (SELECT AVG(e2.salary) FROM employes e2 where e2.title = e.title);
