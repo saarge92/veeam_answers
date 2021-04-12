45833e6851f0e480205e4d3822b4aa98197a5854
sarage92@mail.ru <C#language`1>
2021-04-12 19:28:28 +0400

    4 task updated

--- 4.php
+++ 4.php
@@ -4,12 +4,24 @@ $sortArray = [5, 7, 11, 15];
 
 // Исходное число поиска
 $sourceNumber = 6;
+$countElementToFind = 3;
 
-$sortedArrayFilter = array_filter($sortArray, function (int $element) use ($sourceNumber) {
-    return $element > $sourceNumber;
-});
 
-// Берем первые три числа, удовлетворяющих поиску
-$result = array_slice($sortedArrayFilter, 0, 3);
+function search(array $searchArray, int $sourceNumber, int $countToSearch): array
+{
+    $resultArray = [];
+    foreach ($searchArray as $element) {
+        if ($element > $sourceNumber) {
+            if (count($resultArray) < 3) {
+                $resultArray[] = $element;
+            } else
+                break;
+
+        }
+    }
+    return $resultArray;
+}
+
+$result = search($sortArray, 7, 3);
 
 print_r($result);
